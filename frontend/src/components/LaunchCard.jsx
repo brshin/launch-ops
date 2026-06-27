85f6ce82c16780dc4fe1f6cc3d2617cf4a53b682
Brandon Shin <143565532+brshin@users.noreply.github.com>
2026-06-27 16:16:28 -0700

    feat: add expandable LaunchCard details

--- frontend/src/components/LaunchCard.jsx
+++ frontend/src/components/LaunchCard.jsx
@@ -1,20 +1,36 @@
 import { useState } from 'react';
 
 export default function LaunchCard({ launch }) {
+    const [isExpanded, setIsExpanded] = useState(false);
 
     return (
         <div>
-            <h2 className="text-xl font-semibold">{launch.name}</h2>
-            <p className="text-gray-400">Scheduled Launch Time: {new Date(launch.net).toLocaleString("en-US", {
-              month: "short",
-              day: "numeric",
-              year: "numeric",
-              hour: "2-digit",
-              minute: "2-digit",
-            })}</p>
+            {/* Overview Section */}
+            <div>
+                <h2 className="text-xl font-semibold">{launch.name}</h2>
+                <p className="text-gray-400">Scheduled Launch Time: {new Date(launch.net).toLocaleString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                })}
+                </p>
+                <button onClick={() => setIsExpanded(!isExpanded)}>
+                    {isExpanded ? 'Close' : 'More'}
+                </button>
+            </div>
+            {/* Expanded Section */}
+            {isExpanded && (
+                <div>
+                    <h2>Now Expanded.</h2>
+                </div>
+            )}
         </div>
     );
 }
+
+
 /*
         <div>
             <h2 className="text-xl font-semibold">{launch.name}</h2>
