7906417c2bb0b1183adfa6cf7f09f147602f2cc3
Brandon Shin <143565532+brshin@users.noreply.github.com>
2026-06-27 20:24:56 -0700

    feat: expand LaunchCard with detailed mission and pad info

--- frontend/src/components/LaunchCard.jsx
+++ frontend/src/components/LaunchCard.jsx
@@ -8,6 +8,7 @@ export default function LaunchCard({ launch }) {
             {/* Overview Section */}
             <div>
                 <h2 className="text-xl font-semibold">{launch.name}</h2>
+                <p>{launch.launch_service_provider.name}</p>
                 <p className="text-gray-400">Scheduled Launch Time: {new Date(launch.net).toLocaleString("en-US", {
                     month: "short",
                     day: "numeric",
@@ -16,6 +17,7 @@ export default function LaunchCard({ launch }) {
                     minute: "2-digit",
                 })}
                 </p>
+                <img src={launch.image.image_url}></img>
                 <button onClick={() => setIsExpanded(!isExpanded)}>
                     {isExpanded ? 'Close' : 'More'}
                 </button>
@@ -23,9 +25,33 @@ export default function LaunchCard({ launch }) {
             {/* Expanded Section */}
             {isExpanded && (
                 <div>
-                    <h2>Now Expanded.</h2>
+                    <h3>Pad: {launch.pad.name}</h3>
+                    <p>Last Updated: {launch.last_updated}</p>
+                    <p>Status: {launch.status.abbrev}</p>
+                    <p>Window: {launch.window_start} - {launch.window_end}</p>
+
+                    <h3>Mission: {launch.mission.name}</h3>
+                    <p>{launch.mission.orbit.name}</p>
+                    <p>{launch.mission.description}</p>
+
                 </div>
             )}
         </div>
     );
 }
+
+/*
+    apiId: launch.id,
+    //name: launch.name,
+    //status: launch.status,
+    //last_updated: launch.last_updated,
+    //net: launch.net,
+    net_precision: launch.net_precision,
+    //window_start: launch.window_start,
+    //window_end: launch.window_end,
+    //image: launch.image,
+    //launch_service_provider: launch.launch_service_provider,
+    //rocket: launch.rocket,
+    //mission: launch.mission,
+    pad: launch.pad
+*/
